689276b4bb66af5274a11554a369c1713b073061
Vaidotas <vaidotas.eigelis@gmail.com>
2018-01-30 15:36:25 +0200

    Code 06

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ivesk svori KG: ");
+        float svoris = sc.nextFloat();
+        System.out.println("Ivesk ugi m; ");
+        float ugis = sc.nextFloat();
+
+        float KMI = svoris / (ugis * ugis);
+
+        System.out.println("KMI: " + KMI);
     }
 }
